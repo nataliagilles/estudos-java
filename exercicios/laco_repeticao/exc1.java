1cebad09319e64999f044d3caa044ca5df1ab8e0
Natalia Gilles <nataliarodriguesgilles@gmail.comclear>
2026-01-22 23:09:52 -0300

    treinamento de while no Java

---
+++ exercicios/laco_repeticao/exc1.java
@@ -0,0 +1,21 @@
+package exercicios.laco_repeticao;
+
+import java.util.Scanner;
+
+public class exc1 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+}
